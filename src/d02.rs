b8746ac0cf33bd4ea4815463fadf2f692f78994f
Malcolm Hall <malcolm.r.hall@outlook.com>
2022-04-25 10:08:55 +0100

    solution for day 2 part 2

--- src/d02.rs
+++ src/d02.rs
@@ -1,52 +1,116 @@
 use std::fs;
 
 pub fn d02_solutions() {
-    let input = fs::read_to_string("input/d02.txt").unwrap();
-    let lines = input.lines().map(|line| line.to_owned());
-    let box_dimensions: Vec<BoxDimensions> = lines.map(|line| parse_box_dimensions(&line)).collect();
-    let total_area = part_1(box_dimensions);
-    println!("The elves should order {total_area} square feet of wrapping paper")
+    let all_boxes = parse_input("input/d02.txt");
+    let total_wrapping_paper = part_1(&all_boxes);
+    let total_ribbon = part_2(&all_boxes);
+    println!("The elves should order {total_wrapping_paper} square feet of wrapping paper");
+    println!("The elves should order {total_ribbon} feet of ribbon");
 }
 
-type BoxDimensions = Vec<u32>;
 type Area = u32;
+type Length = u32;
 
-fn part_1(box_dimensions: Vec<BoxDimensions>) -> Area {
-    box_dimensions.iter()
-        .map(|dimensions| get_wrapping_paper_area(&dimensions))
+#[derive(PartialEq, Debug)]
+struct OrderedSides {
+    min: u32,
+    mid: u32,
+    max: u32,
+}
+
+fn parse_input(path: &str) -> Vec<OrderedSides> {
+    let input = fs::read_to_string(path).unwrap();
+    let dimensions = input.lines().map(|line| line.to_owned());
+    dimensions.map(|line| parse_box_dimensions(&line)).collect()
+}
+
+fn parse_box_dimensions(dimensions: &str) -> OrderedSides {
+    let mut sides = dimensions
+        .split('x')
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+    sides.sort_unstable();
+
+    assert!(sides.len() == 3, "Expected 3 dimensions");
+
+    OrderedSides {
+        min: sides[0],
+        mid: sides[1],
+        max: sides[2],
+    }
+}
+
+fn part_1(all_boxes: &Vec<OrderedSides>) -> Area {
+    all_boxes
+        .iter()
+        .map(|ordered_sides| get_wrapping_paper_area(&ordered_sides))
         .sum::<Area>()
 }
 
-fn parse_box_dimensions(dimensions: &str) -> BoxDimensions {
-    dimensions.split('x').map(|s| s.parse().unwrap()).collect()
+fn part_2(all_boxes: &Vec<OrderedSides>) -> Length {
+    all_boxes
+        .iter()
+        .map(|ordered_sides| get_ribbon_length(&ordered_sides))
+        .sum::<Length>()
 }
 
-fn get_wrapping_paper_area(dimensions: &BoxDimensions) -> Area {
-    assert!(dimensions.len() == 3, "Expected 3 dimensions");
-    let (l, w, h) = (dimensions[0], dimensions[1], dimensions[2]);
-    let areas = vec![l * w, w * h, h * l];
-    let slack = areas.iter().min().unwrap();
-    2 * areas.iter().sum::<u32>() + slack
+fn get_wrapping_paper_area(ordered_sides: &OrderedSides) -> Area {
+    let OrderedSides { min, mid, max } = ordered_sides;
+    3 * (min * mid) + 2 * (mid * max + max * min)
+}
+
+fn get_ribbon_length(ordered_sides: &OrderedSides) -> Length {
+    let OrderedSides { min, mid, max } = ordered_sides;
+    2 * (min + mid) + min * mid * max
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn parse_box_dimensions_examples() {
-        assert_eq!(parse_box_dimensions("2x3x4"), vec![2, 3, 4]);
-        assert_eq!(parse_box_dimensions("1x1x10"), vec![1, 1, 10]);
+    fn examples() -> Vec<OrderedSides> {
+        vec![
+            OrderedSides {
+                min: 2,
+                mid: 3,
+                max: 4,
+            },
+            OrderedSides {
+                min: 1,
+                mid: 1,
+                max: 10,
+            },
+        ]
     }
 
     #[test]
-    fn get_wrapping_paper_area_examples() {
-        assert_eq!(get_wrapping_paper_area(&vec![2, 3, 4]), 58);
-        assert_eq!(get_wrapping_paper_area(&vec![1, 1, 10]), 43);
+    fn parse_box_dimensions_examples() {
+        let examples = examples();
+        assert_eq!(parse_box_dimensions("2x3x4"), examples[0]);
+        assert_eq!(parse_box_dimensions("1x1x10"), examples[1]);
     }
 
     #[test]
     fn part_1_examples() {
-        assert_eq!(part_1(vec![ vec![2,3,4], vec![1, 1, 10] ]), 58 + 43);
+        assert_eq!(part_1(&examples()), 58 + 43);
+    }
+
+    #[test]
+    fn part_2_examples() {
+        assert_eq!(part_2(&examples()), 34 + 14);
+    }
+
+    #[test]
+    fn get_wrapping_paper_area_examples() {
+        let examples = examples();
+        assert_eq!(get_wrapping_paper_area(&examples[0]), 58);
+        assert_eq!(get_wrapping_paper_area(&examples[1]), 43);
+    }
+
+    #[test]
+    fn get_ribbon_length_examples() {
+        let examples = examples();
+        assert_eq!(get_ribbon_length(&examples[0]), 34);
+        assert_eq!(get_ribbon_length(&examples[1]), 14);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use aoc_2015::{d01, d02};
 
-
 fn main() {
     d01::d01_solutions();
     d02::d02_solutions();
